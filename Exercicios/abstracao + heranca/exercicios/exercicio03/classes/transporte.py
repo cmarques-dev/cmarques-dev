4d7d3764499fcb9e767a6acebf737eb93ff79b9d
Carlos Marques <gcarlosmarques@icloud.com>
2026-08-18 16:24:16 -0300

    Delete Exercicios directory

--- Exercicios/abstracao + heranca/exercicios/exercicio03/classes/transporte.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-class Transporte(ABC):
-    def __init__(self, distancia):
-        self.distancia = distancia
-
-    @abstractmethod
-    def cal_frete(self):
-        pass
